53bd7d6a709f791ee1d13d70dffafa4c22e8542f
jakesorel <Coexist-97>
2020-07-30 23:00:38 +0100

    1st push

---
+++ for_loopCPM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                                                                                                                           
+
+n_iter=$1
+n_param_step=$2
+N_job=$3
+
+for i in {0..$N_job}
+do
+    sbatch run_jobCPM.sh "$1" "$2" "$3" "$i"
+done
+
---
+++ run_jobCPM.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=4:00:00   # walltime
+#SBATCH --ntasks=20   # number of processor cores (i.e. tasks)
+#SBATCH -J "cell_sorting_jakecs"   # job name
+#SBATCH --output=output.out
+#SBATCH --error=output.out   
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=jakecs@caltech.edu
+
+source activate apical_domain
+
+python CPM/CPM_ND_cluster "$1" "$2" "$3" "$4"
