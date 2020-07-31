f82bb08dca7caa19e212bf443a3ec62f77623350
jakesorel <Coexist-97>
2020-07-31 12:47:28 +0100

    1st push

--- CPM/CPM_ND_cluster.py
+++ CPM/CPM_ND_cluster.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     rep_space = np.arange(n_iter)
     PP,RR,BB,TT,NN = np.meshgrid(p0_space, r0_space, beta_space, T_space,rep_space,indexing="ij")
     inputs = np.array([PP.ravel()[i_job::N_job],RR.ravel()[i_job::N_job],BB.ravel()[i_job::N_job],TT.ravel()[i_job::N_job],np.arange(NN.size)[i_job::N_job]]).T
-    n_slurm_tasks = 8#int(os.environ["SLURM_NTASKS"])
+    n_slurm_tasks = int(os.environ["SLURM_NTASKS"])
     client = Client(threads_per_worker=1, n_workers=n_slurm_tasks)
     lazy_results = []
     for inputt in inputs:
