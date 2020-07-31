f7b00fe072a60c20d3196f6ff5809fad3049781f
jakesorel <Coexist-97>
2020-07-31 12:34:53 +0100

    1st push

--- run_jobCPM.sh
+++ run_jobCPM.sh
@@ -3,7 +3,7 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=4:00:00   # walltime
-#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=64   # number of processor cores (i.e. tasks)
 #SBATCH -J "cell_sorting_jakecs"   # job name
 #SBATCH --output=output.out
 #SBATCH --error=output.out   
