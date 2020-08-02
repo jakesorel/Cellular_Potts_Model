405ddedc45418f5a7cbc7ffda099d4c773adbda0
jakesorel <Coexist-97>
2020-08-02 12:38:55 +0100

    1st push

--- CPM/CPM.py
+++ CPM/CPM.py
@@ -706,7 +706,7 @@ class CPM:
                 for k in range(len(self.xy_clls_tup)):
                     try:
                         I = do_step(I)
-                    except AttributeError or IndexError: #If cells disappear, freeze all cells for subsequent frames. This is picked up in post-analysis
+                    except AttributeError or IndexError or TypeError: #If cells disappear, freeze all cells for subsequent frames. This is picked up in post-analysis
                         break
                 if i_save[ni]:
                     print(np.round(ni/n_steps * 100),"%")
