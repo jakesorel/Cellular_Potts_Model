8618edaaeaf3790daaf7500ae49006fa2f1d3c85
jakesorel <jake.cornwallscoones@crick.ac.uk>
2022-04-26 21:58:04 +0100

    modified

--- CPM2/run_analysis.py
+++ CPM2/run_analysis.py
@@ -96,6 +96,15 @@ if __name__ == "__main__":
     if not os.path.exists("results/compiled/scrambled"):
         os.mkdir("results/compiled/scrambled")
 
+
+    if not os.path.exists("results/compiled/soft"):
+        os.mkdir("results/compiled/soft")
+
+
+    if not os.path.exists("results/compiled/stiff"):
+        os.mkdir("results/compiled/stiff")
+
+
     #
     # I_save_sparse = cPickle.load(bz2.BZ2File("results/bootstrap/%d.pbz2"%iter_i, 'rb'))
     #
@@ -105,12 +114,30 @@ if __name__ == "__main__":
     #               "E_ex":next[:,0],"T_ex":next[:,1],"X_ex":next[:,2]})
     # df.to_csv("results/compiled/bootstrap/%d.csv"%iter_i)
     #
+    #
+    #
+    # I_save_sparse = cPickle.load(bz2.BZ2File("results/scrambled/%d.pbz2"%iter_i, 'rb'))
+    #
+    # cc,next = get_top_values_t(I_save_sparse)
+    #
+    # df = pd.DataFrame({"t":np.arange(cc.shape[0])*1e4,"E_cc":cc[:,0],"T_cc":cc[:,1],"X_cc":cc[:,2],
+    #               "E_ex":next[:,0],"T_ex":next[:,1],"X_ex":next[:,2]})
+    # df.to_csv("results/compiled/scrambled/%d.csv"%iter_i)
+    #
 
-
-    I_save_sparse = cPickle.load(bz2.BZ2File("results/scrambled/%d.pbz2"%iter_i, 'rb'))
+    I_save_sparse = cPickle.load(bz2.BZ2File("results/soft/%d.pbz2"%iter_i, 'rb'))
 
     cc,next = get_top_values_t(I_save_sparse)
 
     df = pd.DataFrame({"t":np.arange(cc.shape[0])*1e4,"E_cc":cc[:,0],"T_cc":cc[:,1],"X_cc":cc[:,2],
                   "E_ex":next[:,0],"T_ex":next[:,1],"X_ex":next[:,2]})
-    df.to_csv("results/compiled/scrambled/%d.csv"%iter_i)
+    df.to_csv("results/compiled/soft/%d.csv"%iter_i)
+
+
+    I_save_sparse = cPickle.load(bz2.BZ2File("results/stiff/%d.pbz2"%iter_i, 'rb'))
+
+    cc,next = get_top_values_t(I_save_sparse)
+
+    df = pd.DataFrame({"t":np.arange(cc.shape[0])*1e4,"E_cc":cc[:,0],"T_cc":cc[:,1],"X_cc":cc[:,2],
+                  "E_ex":next[:,0],"T_ex":next[:,1],"X_ex":next[:,2]})
+    df.to_csv("results/compiled/stiff/%d.csv"%iter_i)
