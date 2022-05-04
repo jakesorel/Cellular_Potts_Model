7c2f1441f8340041a3762f45edd610fbd9661687
jakesorel <jake.cornwallscoones@crick.ac.uk>
2022-05-04 16:40:26 +0100

    modified

--- CPM2/compiled_analysis.py
+++ CPM2/compiled_analysis.py
@@ -3,6 +3,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import matplotlib
+
+matplotlib.rcParams['pdf.fonttype'] = 42
+
 
 def load_compiled_data(folder):
     files = os.listdir(folder)[:500]
@@ -10,7 +14,7 @@ def load_compiled_data(folder):
     for file in files:
         index = int(file.split(".csv")[0])
         df = pd.read_csv(folder+"/"+file,index_col=0)
-        df = df[::25]
+        df = df[::100]
         df["sim"] = index
         df_all = pd.concat((df_all,df))
     df_all.index = np.arange(df_all.shape[0])
@@ -56,35 +60,84 @@ fig.show()
 
 
 soft_df = load_compiled_data("results/compiled/soft")
-soft_df["X_external"] = soft_df["X_ex"] == soft_df["N_X"]
+soft_df["X_external"] = soft_df["X_ex2"] == soft_df["N_X"]
 # soft_df["X_external"] = soft_df["X_ex3"] == 6
 soft_df["N"] = soft_df["N_X"] +soft_df["N_E"] + soft_df["N_T"]
-
 soft_df["E_external"] = soft_df["E_ex3"] == soft_df["N_E"]
 soft_df["T_external"] = soft_df["T_ex3"] == soft_df["N_T"]
-
 stiff_df = load_compiled_data("results/compiled/stiff")
-stiff_df["X_external"] = stiff_df["X_ex"] == stiff_df["N_X"]
+stiff_df["X_external"] = stiff_df["X_ex2"] == stiff_df["N_X"]
 # stiff_df["X_external"] = stiff_df["X_ex3"] == 6
 stiff_df["N"] = stiff_df["N_X"] +stiff_df["N_E"] + stiff_df["N_T"]
-
-
 stiff_df["E_external"] = stiff_df["E_ex3"] == stiff_df["N_E"]
 stiff_df["T_external"] = stiff_df["T_ex3"] == stiff_df["N_T"]
 
-fig, ax = plt.subplots()
-sns.lineplot(x="t",y="E_cc",data=soft_df,ax=ax,label="Soft")
-sns.lineplot(x="t",y="E_cc",data=stiff_df,ax=ax,label="Stiff")
-# sns.lineplot(x="t",y="E_cc",data=soft_df,ax=ax,label="Soft")
-# sns.lineplot(x="t",y="E_cc",data=stiff_df,ax=ax,label="Stiff")
-# ax.set(xlim=(0,3e6))
+soft_df["t_million"] = soft_df["t"]/1e6
+stiff_df["t_million"] = stiff_df["t"]/1e6
 
-# sns.lineplot(x="t",y="X_external",data=bootstrap_df,ax=ax)
+fig, ax = plt.subplots(figsize=(6,4))
+sns.lineplot(x="t_million",y="X_external",data=soft_df,ax=ax,label="Soft",color="#158701")
+sns.lineplot(x="t_million",y="X_external",data=stiff_df,ax=ax,label="Stiff",color="grey")
+ax.set(ylabel="Fraction of XEN external",xlabel="Time (x"r"$10^6$"" MCS)")
+fig.subplots_adjust(bottom=0.3,top=0.8,left=0.3,right=0.8)
+fig.savefig("plots/Fraction XEN external.pdf",dpi=300)
+
+stiff_df["E_sorted"] = stiff_df["E_cc"]==1
+soft_df["E_sorted"] = soft_df["E_cc"]==1
+
+stiff_df["T_sorted"] = stiff_df["T_cc"]==1
+stiff_df["ET_sorted"] = (stiff_df["E_sorted"] )*(stiff_df["T_sorted"] )
+stiff_df["ETX_sorted"] = (stiff_df["E_sorted"] )*(stiff_df["T_sorted"] )*stiff_df["X_external"]
+
+stiff_df_fin = stiff_df[1000::1001]
+
+
+fig, ax = plt.subplots(figsize=(6,4))
+sns.lineplot(x="t_million",y="E_sorted",data=stiff_df,ax=ax,label="E",color="#158701")
+sns.lineplot(x="t_million",y="T_sorted",data=stiff_df,ax=ax,label="T",color="grey")
+sns.lineplot(x="t_million",y="ET_sorted",data=stiff_df,ax=ax,label="ET",color="grey")
+fig.subplots_adjust(bottom=0.3,top=0.8,left=0.3,right=0.8)
 fig.show()
 
 
-soft_df = load_compiled_data("results/compiled/soft")
-stiff_df = load_compiled_data("results/compiled/stiff")
-print((soft_df[soft_df["t"]==soft_df["t"].max()]["X_ex"]==6).mean())
-print((stiff_df[stiff_df["t"]==stiff_df["t"].max()]["X_ex"]==6).mean())
+t_span = stiff_df["t"].values
 
+
+fig, ax = plt.subplots()
+sns.violinplot(ax=ax,data=soft_df[1000::1001],y="X_ex2")
+fig.show()
+
+
+
+
+def calculate_bootstrap_ci(vals):
+    return sns.utils.ci(sns.algorithms.bootstrap(vals))
+
+def calculate_mean_ci_for_t(df,column,t):
+    dfi = df[df["t"]==t]
+    vals = dfi[column].values
+    return vals.mean(),calculate_bootstrap_ci(vals)
+
+def calculate_mean_ci_for_ti(df,column,ti,n=1001):
+    dfi = df[ti::n]
+    vals = dfi[column].values
+    return vals.mean(),calculate_bootstrap_ci(vals)
+
+
+
+t_span = stiff_df["t"].values
+
+soft_mean,soft_ci = np.zeros(1001),np.zeros((1001,2))
+
+for ti in np.arange(1001):
+    soft_mean[ti],soft_ci[ti] = calculate_mean_ci_for_ti(soft_df,"X_external",ti)
+    print(ti/1001 * 100, "%")
+
+
+from scipy.signal import savgol_filter
+
+soft_mean_smooth = savgol_filter(soft_mean,101,3)
+
+
+soft_mean,soft_ci = np.array([calculate_mean_ci_for_ti(soft_df,"X_external",ti) for ti in np.arange(0,1001)]).T
+soft_mean,soft_ci = np.array(list(soft_mean)),np.array(list(soft_ci))
---
+++ CPM2/run_softstiff_simulation2.py
@@ -0,0 +1,133 @@
+import numpy as np
+from cpm import CPM
+import matplotlib.pyplot as plt
+import time
+import os
+import sys
+
+
+if __name__ == "__main__":
+
+    if not os.path.exists("results"):
+        os.mkdir("results")
+
+    if not os.path.exists("results/soft"):
+        os.mkdir("results/soft")
+
+    if not os.path.exists("results/plots"):
+        os.mkdir("results/plots")
+
+    if not os.path.exists("results/plots/soft"):
+        os.mkdir("results/plots/soft")
+
+    if not os.path.exists("results/stiff"):
+        os.mkdir("results/stiff")
+
+
+    if not os.path.exists("results/plots/scrambled"):
+        os.mkdir("results/plots/scrambled")
+
+    if not os.path.exists("results/scrambled"):
+        os.mkdir("results/scrambled")
+
+
+    if not os.path.exists("results/plots/stiff"):
+        os.mkdir("results/plots/stiff")
+
+    iter_i = int(sys.argv[1])
+
+    A0 = 30
+    P0 = 0
+    lambda_A = 1
+    lambda_P = 0.2
+    b_e = -0.5
+
+    W = np.array([[b_e,b_e,b_e,b_e],
+                  [b_e,1.911305,0.494644,0.505116],
+                  [b_e,0.494644,2.161360,0.420959],
+                  [b_e,0.505116,0.420959,0.529589]])*6.02
+
+
+    params = {"A0":[A0,A0,A0],
+              "P0":[P0,P0,P0],
+              "lambda_A":[lambda_A,lambda_A,lambda_A],
+              "lambda_P":[lambda_P,lambda_P,lambda_P],
+              "W":W,
+              "T":15}
+    cpm = CPM(params)
+    cpm.make_grid(100,100)
+    cpm.generate_cells(N_cell_dict={"E": 8, "T": 8,"X":6})
+    cpm.make_init("circle", np.sqrt(params["A0"][0] / np.pi) * 0.8, np.sqrt(params["A0"][0] / np.pi) * 0.2)
+    # adhesion_vals_full = np.load("../adhesion_matrices/%i.npz" % iter_i).get("adhesion_vals")
+    adhesion_vals_full = np.load("../adhesion_matrices/%i.npz" % iter_i).get("adhesion_vals")
+    # adhesion_vals_full = np.load("adhesion_matrices/%i.npz" % iter_i).get("adhesion_vals")
+
+    adhesion_vals_full[0] = b_e*cpm.lambda_P
+    adhesion_vals_full[:,0] = b_e*cpm.lambda_P
+    adhesion_vals_full[0,0] = 0
+    cpm.J = -adhesion_vals_full * 6
+    cpm.get_J_diff()
+    t0 = time.time()
+    cpm.simulate(int(1e7), int(1000), initialize=True, J0=-8)
+
+    # t1 = time.time()
+    cpm.save_simulation("results/stiff",str(iter_i))
+
+    fig, ax = plt.subplots()
+    ax.imshow(cpm.generate_image(cpm.I, res=8, col_dict={1: "red", 2: "blue", 3: "green"}))
+    ax.axis("off")
+    fig.savefig("results/plots/stiff/%d.pdf"%iter_i,dpi=300)
+    # print(t1-t0)
+    # cpm.generate_image_t(res=4,col_dict={1:"red",2:"blue",3:"green"})
+    # cpm.animate()
+
+    params2 = {"A0":[A0,A0,A0],
+              "P0":[P0,P0,P0],
+              "lambda_A":[lambda_A,lambda_A,lambda_A],
+              "lambda_P":[lambda_P,lambda_P,lambda_P*0.2],
+              "W":W,
+              "T":15}
+    cpm2 = CPM(params2)
+    cpm2.make_grid(100, 100)
+    cpm2.generate_cells(N_cell_dict={"E": 8, "T": 8,"X":6})
+    cpm2.I0 = cpm.I_save[0]
+    cpm2.I = cpm2.I0.copy()
+    cpm2.n_cells = cpm.n_cells
+    cpm2.assign_AP()
+    cpm2.J = cpm.J.copy()
+    cpm2.J[0] = -6*b_e*cpm2.lambda_P
+    cpm2.J[:,0] = -6*b_e*cpm2.lambda_P
+    cpm2.J[0,0] = 0
+    cpm2.J_diff = cpm.J_diff.copy()
+    cpm2.simulate(int(1e7), int(1000), initialize=False)
+    # t1 = time.time()
+    cpm2.save_simulation("results/soft",str(iter_i))
+
+    fig, ax = plt.subplots()
+    ax.imshow(cpm2.generate_image(cpm2.I, res=8, col_dict={1: "red", 2: "blue", 3: "green"}))
+    ax.axis("off")
+    fig.savefig("results/plots/soft/%d.pdf"%iter_i,dpi=300)
+
+    cpm3 = CPM(params)
+    cpm3.make_grid(100, 100)
+    cpm3.generate_cells(N_cell_dict={"E": 8, "T": 8, "X": 6})
+    cpm3.I0 = cpm.I_save[0]
+    cpm3.I = cpm3.I0.copy()
+    cpm3.n_cells = cpm.n_cells
+    cpm3.assign_AP()
+    adhesion_vals_full = np.load("../adhesion_matrices_scrambled/%i.npz" % iter_i).get("adhesion_vals")
+    # adhesion_vals_full = np.load("adhesion_matrices/%i.npz" % iter_i).get("adhesion_vals")
+
+    adhesion_vals_full[0] = b_e*cpm3.lambda_P
+    adhesion_vals_full[:,0] = b_e*cpm3.lambda_P
+    adhesion_vals_full[0,0] = 0
+    cpm3.J = -adhesion_vals_full * 6
+    cpm3.get_J_diff()
+    cpm3.simulate(int(1e7), int(1000), initialize=False)
+    # t1 = time.time()
+    cpm3.save_simulation("results/scrambled", str(iter_i))
+
+    fig, ax = plt.subplots()
+    ax.imshow(cpm3.generate_image(cpm3.I, res=8, col_dict={1: "red", 2: "blue", 3: "green"}))
+    ax.axis("off")
+    fig.savefig("results/plots/scrambled/%d.pdf" % iter_i, dpi=300)
